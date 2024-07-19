7187effac31344e4c7bf95885e8dfff93bee2791
Bayu Abdillah <bayupauli@gmail.com>
2024-07-19 15:06:58 +0700

    disable method onCofiguring

--- src/FitrahDataAccess/Models/FitrahContext.cs
+++ src/FitrahDataAccess/Models/FitrahContext.cs
@@ -20,14 +20,14 @@ namespace FitrahDataAccess.Models
         public virtual DbSet<History> Histories { get; set; } = null!;
         public virtual DbSet<Recap> Recaps { get; set; } = null!;
 
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-            if (!optionsBuilder.IsConfigured)
-            {
-#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Data Source = LAPTOP-26EQ94MU; Initial Catalog= Fitrah; Trusted_Connection = True; User = basilisktf; Password = basilisk; TrustServerCertificate = True");
-            }
-        }
+//         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+//         {
+//             if (!optionsBuilder.IsConfigured)
+//             {
+// #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+//                 optionsBuilder.UseSqlServer("Data Source = LAPTOP-26EQ94MU; Initial Catalog= Fitrah; Trusted_Connection = True; User = basilisktf; Password = basilisk; TrustServerCertificate = True");
+//             }
+//         }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
